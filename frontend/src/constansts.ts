f2d4f50bf4c69a2b3875c1c424edbd34572383ce
arslanbekbolotov <arslanbekbolotov58@gmail.com>
2024-01-31 15:00:04 +0600

    updated base_url

--- frontend/src/constansts.ts
+++ frontend/src/constansts.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.BASE_URL;
-export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const BASE_URL = 'https://cocktails-app-api.vercel.app/';
+export const GOOGLE_CLIENT_ID ='51962709179-npcp3jvdvei5ea8o28gbe64pnfpueltk.apps.googleusercontent.com';
